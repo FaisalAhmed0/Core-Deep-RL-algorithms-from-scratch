718f65e15f805fb14e41d09b70892f65fe9b9178
Faisal <faisal.ahmed.a.m@gmail.com>
2023-10-21 15:11:19 +0000

    fixed shape

--- DQN/agents.py
+++ DQN/agents.py
@@ -102,10 +102,12 @@ class DQN_Agent:
             if self.target_network:
               if self.double:
                 argmax_action = self.dqn_model(next_observations).argmax(dim=1).reshape(-1)
-                print(f"argmax_action.shape:{argmax_action.shape}")
-                print(f"rewards.shape:{rewards.shape}")
-                print(f"dones.shape:{dones.shape}")
-                targets = rewards + self.gamma*((self.target_dqn_model(next_observations)[argmax_action]).detach() * dones)
+                # print(f"argmax_action.shape:{argmax_action.shape}")
+                # print(f"rewards.shape:{rewards.shape}")
+                # print(f"dones.shape:{dones.shape}")
+                # print(f"next_observations.shape:{next_observations.shape}")
+                # print(f"self.target_dqn_model(next_observations):{self.target_dqn_model(next_observations).shape}")
+                targets = rewards + self.gamma*((self.target_dqn_model(next_observations)[:, argmax_action]).detach() * dones)
               else:
                 targets = rewards + self.gamma*((self.target_dqn_model(next_observations).max(dim=1)[0]).detach() * dones)
             else:
